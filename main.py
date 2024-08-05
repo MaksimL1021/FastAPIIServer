d213cd7e32b07aa4552ae39327db471758451e0e
nisuev <nisuev04@mail.ru>
2024-08-05 19:39:13 +0300

    modified main.py

--- main.py
+++ main.py
@@ -217,4 +217,4 @@ async def forgot_password_step3(request: PasswordResetStep3):
 # Запуск приложения
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
